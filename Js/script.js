ae9cdef97547b1e75112e42a7df85680be8116c2
Mahsa Iri <mahsairi1998@gmail.com>
2023-10-09 10:51:57 +0200

    I added event to my buttons and I am going to develop the event function further1

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,6 @@
 const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
+const button = document.querySelectorAll(".filter")
 
 const searchHandler = (event)=> {
     const searchValue = event.target.value.toLowerCase().trim();
@@ -13,4 +14,10 @@ const searchHandler = (event)=> {
     });
 };
 
-searchInput.addEventListener("keyup", searchHandler)
+const filterHandler = (event) => {
+    const filter = event.target.dataset.filter;
+}
+searchInput.addEventListener("keyup", searchHandler);
+buttons.forEach ( button =>{
+    button.addEventListener ("click" ,filterHandler);
+} )
